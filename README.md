d8d567f47ee387769412f5a69aac2f39444670a0
重王泉 <1813114234@qq.com>
2020-03-08 17:33:06 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# vant 组件 px转换成rpx
+src: 未转换的组件
+dist: 转换后后的
+
+
---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const pxtorem = require('postcss-pxtransform');
+
+
+
+// 复制文件到dist ,除了wxss文件
+gulp.task('copy', function(){
+    return gulp.src(["src/**/*.**","!src/**/*.wxss"])  
+      .pipe(gulp.dest("dist"))
+})
+
+// wxss文件中px转换成rpx，且都乘2。并复制wxss文件到dist
+gulp.task('pxTorpx', function(){
+  let options={
+      //平台类型
+      platform: 'weapp',
+      //设备宽度 对应iphone 6
+      designWidth: 750,
+      //转换比例 覆盖插件默认比例
+      deviceRatio:{
+        '750':0.5
+      }
+    }
+    let processors = [
+        pxtorem(options)
+    ];
+      return gulp.src('src/**/*.wxss')
+          .pipe(postcss(processors))
+          .pipe(gulp.dest('dist'));
+})
+
+// 多任务依次执行
+gulp.task('default', gulp.series('copy','pxTorpx'))
